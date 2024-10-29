072bf10cefe31311127dd564eabdb5695a6f8383
shreesh <shreesht0@gmail.com>
2024-10-29 11:07:38 +0530

    first commit

--- companiesProjects/restApi_App/view/employee.py
+++
@@ -1,7 +0,0 @@
-from ..serializers import EmployeeSerializers
-from ..models.employee import Employee
-from rest_framework import viewsets
-
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializers
